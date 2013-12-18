69ea0e8434b301c7693c627db9bf3108d70d0344
Jasper <Jasper@192.168.100.28>
2013-12-18 22:16:13 +0100

    Album integration in articles

--- views/articles/index.php
+++ views/articles/index.php
@@ -1,10 +1,12 @@
 <h1>Articles</h1>
+<a href="<?php echo URL; ?>articles/create">Create New Article</a><br/><br/>
 <table>
-    <tr><td>Id: </td><td>Title: </td></tr>
+    <tr><td>Id: </td><td>Title: </td><td>Category: </td></tr>
     <?php foreach ($this->articles as $article) { ?>
         <tr>
             <td><?php echo $article['id']; ?></td>
             <td><?php echo $article['title']; ?></td>
+            <td><?php echo $article['category']; ?></td>
             <td>
                 <a href="<?php echo URL; ?>articles/edit/<?php echo $article['id']; ?>">Edit</a>
             </td>
